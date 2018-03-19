2b050b27bcf180309d28eb0bc56b9823b2a026b8
Sam Ebertz <samuel.ebertz@gmail.com>
2018-03-19 11:24:00 -0500

    updated ring and winged_edge [refactor]
    removed some functionality from Ring and commented out old BFEV
    components in winged_edge.js
    
    just a follow-up to refactor in 716d6663e8e5228bfc26df7252939fde565c71e1

--- js/winged_edge/winged_edge.js
+++ js/winged_edge/winged_edge.js
@@ -1,6 +1,9 @@
-const ring = require('../ring')
-const Node = ring.Node
-const Ring = ring.Ring
+const Node = require('./node')
+const Ring = require('./ring')
+
+const Face = require('./face')
+const Edge = require('./edge')
+const Vertex = require('./vertex')
 
 const TYPES = {
   BODY: -1,
@@ -8,31 +11,35 @@ const TYPES = {
   EDGE: 1,
   VERTEX: 2
 }
-function Face() {
-  // type
-  // PED
-  // NFACE, PFACE
-}
-function Edge() {
-  // type
-  // NFACE, PFACE, NVT, PVT
-  // NED, PED
-  // NCW, PCW, NCCW, PCCW
-}
-function Vertex() {
-  // type
-  // PED
-  // NVT, PVT
-}
 
-function WING() {}
+// function Face() {
+//   this.type = TYPES.FACE
+//   this.nface = this.pface = null
+//   this.ped = null
+// }
 
-function WingedEdge() {
-  this.faces = new Ring()
-  this.edges = new Ring()
-  this.vertices = new Ring()
-}
-WingedEdge.prototype.f = function() {
+// function Edge() {
+//   this.type = TYPES.EDGE
+//   this.nface = this.pface = null
+//   this.ned = this.ped = null
+//   this.nvt = this.pvt = null
+//   this.ncw = this.pcw = null
+//   this.nccw = this.pccw = null
+// }
+
+// function Vertex(x, y, z) {
+//   this.x = x
+//   this.y = y
+//   this.z = z
+//   this.type = TYPES.VERTEX
+//   this.ped = null
+//   this.nvt = this.pvt = null
+// }
+
+function WingedEdge(body) {
+  this.faces = new Ring(body)
+  this.edges = new Ring(body)
+  this.vertices = new Ring(body)
 }
 
 module.exports = { WingedEdge, Face, Edge, Vertex , TYPES}
